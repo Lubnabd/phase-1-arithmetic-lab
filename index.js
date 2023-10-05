bb23167a9a1a48a721ff6b51c1024a3c9b9c7a7d
Lubnabd <lubna.abdelkhaleq@gmail.com>
2023-10-05 11:06:30 +0300

    completed assingments

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-
 // Write your code here
+
+//QUESTION 1 
+const num1 = 30;
+const num2 = 32;
+const multiply = num1 * num2;
+console.log(multiply);
+
+//QUESTION 2 
+const random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+console.log(random);
+
+//QUESTION 3 
+let num3 = 16; 
+let num4 = 4;  
+
+const mod = (num3 % num4);
+console.log(mod);
+
+//QUESTION 4 
+const max = Math.max(5, 10, 15, 20);
+console.log(max);
